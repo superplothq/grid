430336a47209fe64a3601a278f723b3155128997
akash <akash@sharefable.com>
2025-11-10 13:47:44 +0530

    Use grid in playground

--- packages/grid/src/index.ts
+++ packages/grid/src/index.ts
@@ -1,1 +1,19 @@
-export {};
+
+class Grid extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    console.log("Grid connected");
+  }
+}
+
+// TODO decide name of the component
+if (document.createElement("dataflow-grid").constructor === HTMLElement) {
+  window.customElements.define("dataflow-grid", Grid);
+}
+
+export default Grid;
+
+// 
--- packages/playground/src/grid.tsx
+++ packages/playground/src/grid.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "grid";
+
+// Declare the custom element for TypeScript
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "dataflow-grid": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
 
 const GridPlayground: React.FC = () => {
+  useEffect(() => {
+    console.log("GridPlayground mounted");
+  }, []);
+
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <p>todo</p>
-    </div>
+    <dataflow-grid />
   );
 };
 
